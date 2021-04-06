afaf6caa0b100b1a9199fe515ebc2aaa6c711f84
Gmishra2000 <mantumisara485@gmail.com>
2021-04-07 00:11:26 +0530

    add

---
+++ script.js
@@ -0,0 +1,30 @@
+const File = require('./models/file');
+
+const fs = require('fs');
+
+const connectDB = require('./config/db');
+connectDB();
+
+async function fetchData() {
+    //24 hours
+    //milli seconds
+    const pastDate = new Date(Date.now() - 1000 * 60 * 60 * 24);
+    const files = await File.find({ createdAt: { $lt: pastDate } });
+
+    if (files.length) {
+        for (const file of files) {
+            try {
+                false.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch (err) {
+                console.log(`error while deleting file ${err}`);
+            }
+        }
+    }
+}
+
+fetchData().then(() => {
+    console.log('Job done!');
+    process.exit();
+})
